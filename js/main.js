0bc9726f7a7d679331f9ef6aa65a6da5e5b3c16b
Justus <37590567+HerrBlauzahn@users.noreply.github.com>
2020-10-31 23:47:44 +0100

    Offline detection

--- js/main.js
+++ js/main.js
@@ -435,29 +435,13 @@ function CoOffline() {
 	history.pushState("","","#offline");
 	document.getElementById("page-offline").hidden = false;
 }
-/*<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
-  <thead>
-    <tr>
-      <th class="mdl-data-table__cell--non-numeric">Material</th>
-      <th>Quantity</th>
-      <th>Unit price</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
-      <td>25</td>
-      <td>$2.90</td>
-    </tr>
-    <tr>
-      <td class="mdl-data-table__cell--non-numeric">Plywood (Birch)</td>
-      <td>50</td>
-      <td>$1.25</td>
-    </tr>
-    <tr>
-      <td class="mdl-data-table__cell--non-numeric">Laminate (Gold on Blue)</td>
-      <td>10</td>
-      <td>$2.35</td>
-    </tr>
-  </tbody>
-</table>*/
+
+var snackbarContainer = document.getElementById('offline-toast');
+window.addEventListener('offline', (event) => {
+	var offMessage = {message:'You appear to be offline.'};
+	snackbarContainer.MaterialSnackbar.showSnackbar(offMessage);
+});
+window.addEventListener('online', (event) => {
+	var offMessage = {message:'You are online.'};
+	snackbarContainer.MaterialSnackbar.showSnackbar(offMessage);
+});
